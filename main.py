44b48eecf46e61f30d29def5a81a0a89a4534ff0
João_Paulo <jpbbarboza1987@gmail.com>
2023-03-19 17:24:10 -0300

    organização do código

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
-# Cadastrando o ativos
+# coding: utf8
+
+# Cadastrando o ativos e especificando a quantidade de entradas
 
 
 entrada = int(input('Digite a quantidade de entradas: '))
 
-ativos = {}
 
+# Ativos sera o banco de dados temporario
+
+dados = {}
+
+#=======================================================================
+
+# Loop para pegar os dados de entrada 
 for i in range(entrada):
 
     ativo = input("Digite o nome do ativo: ")
     valor = float(input("Digite o valor da compra do ativo: "))
     quantidade = int(input("Digite a quantidade comprada: "))
 
-    ativos[ativo] = [valor, quantidade]
+    dados[ativo] = [valor, quantidade]
+    
+#======================================================================
+
 patrimonio = 0
 
+#======================================================================
+
+# Loop que calcula o patrimonio total
 for i in ativos:
 
    patrimonio = patrimonio + ativos[i][0] * ativos[i][1]
 
+#======================================================================
+
 porcentagen = 0
 
-for i in porcentagem:
+#for i in porcentagem:
 
-    porcentagen = patrimonio / ativos[i][0] * ativos[i][1]
+ #   porcentagen = patrimonio / ativos[i][0] * ativos[i][1]
 
 
 print(ativos)
